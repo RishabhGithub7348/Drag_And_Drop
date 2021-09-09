5a0f57781cc844fc5a0ffc0999a293f11557e3be
RishabhGithub7348 <rishabhmaurya7654@gmail.com>
2021-09-09 20:57:28 +0530

    Drag_And_Drop

---
+++ Readme.md
@@ -0,0 +1,3 @@
+This is my Drag_And_Drop  .
+
+Visit the given link to see it  
---
+++ assets/js/main.js
@@ -0,0 +1,10 @@
+// ======= DRAG and DROP =======
+const dropItems = document.getElementById('drop-items')
+
+
+new Sortable(dropItems, {
+    animation: 350,
+    chosenClass: "sortable-chosen",
+    dragClass: "sortable-drag",
+    
+});
